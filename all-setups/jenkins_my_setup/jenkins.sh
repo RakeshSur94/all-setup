658ee000039f76814a9ad84e05afaf66d38c6405
RakeshSur94 <140485395+RakeshSur94@users.noreply.github.com>
2024-06-05 11:40:41 +0530

    Update jenkins.sh

--- all-setups/jenkins_my_setup/jenkins.sh
+++ all-setups/jenkins_my_setup/jenkins.sh
@@ -2,21 +2,16 @@
 #!/bin/bash
 
 # Download and extract Maven 3.9.7
-wget -q https://downloads.apache.org/maven/maven-3/3.9.7/binaries/apache-maven-3.9.7-bin.tar.gz -O /tmp/apache-maven-3.9.7-bin.tar.gz
-sudo tar xf /tmp/apache-maven-3.9.7-bin.tar.gz -C /opt
+wget https://downloads.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz
+tar -xvzf apache-maven-3.8.8-bin.tar.gz
+sudo mv apache-maven-3.8.8 /opt/maven
+
 
 # Set up symbolic link
 sudo ln -sfn /opt/apache-maven-3.9.7 /opt/maven
 
-# Update environment variables
-echo 'export M2_HOME=/opt/maven' >> ~/.bashrc
-echo 'export PATH=$M2_HOME/bin:$PATH' >> ~/.bashrc
-
-# Source .bashrc to apply changes
-source ~/.bashrc
-
-# Verify Maven installation
-mvn -version
+export M2_HOME=/opt/maven
+export PATH=$M2_HOME/bin:$PATH
 
 # Proceed with Jenkins installation (your existing script)
 yum install git java-17-amazon-corretto-jmods.x86_64 -y
